5026d4f13bd67f8dedde6fdd64949b69a41fb752
karur4n <karuran.f@gmail.com>
2017-04-23 23:23:14 +0900

    Add Q01

---
+++ src/clojure_programmer_brain/core.clj
@@ -0,0 +1,8 @@
+(ns clojure-programmer-brain.core
+  (:gen-class)
+  (require [clojure-programmer-brain.q01 :as q01]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (q01/run)))
---
+++ src/clojure_programmer_brain/q01.clj
@@ -0,0 +1,22 @@
+(ns clojure-programmer-brain.q01
+  (require [clojure.string :as s]))
+
+(defn- binarize [n]
+  (Integer/toBinaryString n))
+
+(defn- octalize [n]
+  (Integer/toOctalString n))
+
+(defn- palindrome? [s]
+  (= s (s/reverse s)))
+
+(defn run []
+  "10進数、2進数、8進数のいずれで表現しても回文数となる数のうち、10進数の10以上で最小の値を求めてください。"
+  (let [lis (range 10 (java.lang.Integer/MAX_VALUE))]
+    (first
+      (filter (fn [x]
+        (and
+          (palindrome? (str x))
+          (palindrome? (binarize x))
+          (palindrome? (octalize x))))
+        lis))))
