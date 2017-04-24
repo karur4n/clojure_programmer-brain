986514c04de97eb3d0f540940cc3a4a8f5d3f610
karur4n <karuran.f@gmail.com>
2017-04-25 00:01:15 +0900

    Q01. 該当する数字だけをリスト化する

--- src/clojure_programmer_brain/q01.clj
+++ src/clojure_programmer_brain/q01.clj
@@ -1,23 +1,17 @@
 (ns clojure-programmer-brain.q01
   (require [clojure.string :as s]))
 
-(defn- binarize [n]
-  (Integer/toBinaryString n))
-
-(defn- octalize [n]
-  (Integer/toOctalString n))
-
 (defn- palindrome? [s]
   (= s (s/reverse s)))
 
 (defn run
   "10進数、2進数、8進数のいずれで表現しても回文数となる数のうち、10進数の10以上で最小の値を求めてください。"
   []
-  (let [lis (range 10 (java.lang.Integer/MAX_VALUE))]
+  (let [lis (range 11 java.lang.Integer/MAX_VALUE 2)] ; 10 は回文数にならない
     (first
       (filter (fn [x]
         (and
           (palindrome? (str x))
-          (palindrome? (binarize x))
-          (palindrome? (octalize x))))
+          (palindrome? (Integer/toBinaryString x))
+          (palindrome? (Integer/toOctalString x))))
         lis))))
